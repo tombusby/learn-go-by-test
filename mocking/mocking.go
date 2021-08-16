6d91c37d4712e983bc477f4e74b01d678bc86b0d
Tom Busby <tom@busby.ninja>
2021-08-16 22:22:49 +0200

    Mocking: Add the time.sleep dependency that slows down the tests

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const finalWorld = "Go!"
@@ -11,6 +12,7 @@ const countdownStart = 3
 
 func Countdown(out io.Writer) {
 	for i := countdownStart; i > 0; i-- {
+		time.Sleep(1 * time.Second)
 		fmt.Fprintln(out, i)
 	}
 	fmt.Fprint(out, finalWorld)
