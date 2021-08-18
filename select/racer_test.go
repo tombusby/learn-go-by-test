4116b0044b64131a3b22c671d34daa92367122ee
Tom Busby <tom@busby.ninja>
2021-08-18 19:16:41 +0200

    Select: Use http.NewServer to mock the servers for http requests

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,10 +1,29 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastUrl := "http://www.quii.co.uk"
+
+	slowServer := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			time.Sleep(20 * time.Millisecond)
+			w.WriteHeader(http.StatusOK)
+		}))
+	defer slowServer.Close()
+
+	fastServer := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+		}))
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastUrl := fastServer.URL
 
 	want := fastUrl
 	got := Racer(slowURL, fastUrl)
