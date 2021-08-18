4ffc65e45372f3782d75d5b9172811461381ef79
Tom Busby <tom@busby.ninja>
2021-08-18 20:23:54 +0200

    Select: Allow testing for timeout errors (but tests slow now

--- select/racer_test.go
+++ select/racer_test.go
@@ -9,27 +9,47 @@ import (
 
 func TestRacer(t *testing.T) {
 
-	slowServer := makeDelayedServer(20)
-	fastServer := makeDelayedServer(0)
+	t.Run("compares the speed of servers, returning the url of the fastest one", func(t *testing.T) {
+		slowServer := makeDelayedServer(20 * time.Millisecond)
+		fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+		defer slowServer.Close()
+		defer fastServer.Close()
 
-	slowURL := slowServer.URL
-	fastUrl := fastServer.URL
+		slowURL := slowServer.URL
+		fastUrl := fastServer.URL
 
-	want := fastUrl
-	got := Racer(slowURL, fastUrl)
+		want := fastUrl
+		got, err := Racer(slowURL, fastUrl)
 
-	if got != want {
-		t.Errorf("got %q, want %q", want, got)
-	}
+		if got != want {
+			t.Errorf("got %q, want %q", want, got)
+		}
+
+		if err != nil {
+			t.Error("Got an error when one wasn't expected")
+		}
+	})
+
+	t.Run("returns an error if a server doesn't respond within 10 seconds", func(t *testing.T) {
+		serverA := makeDelayedServer((11 * time.Second))
+		serverB := makeDelayedServer((12 * time.Second))
+
+		defer serverA.Close()
+		defer serverB.Close()
+
+		_, err := Racer(serverA.URL, serverB.URL)
+
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(
 		func(w http.ResponseWriter, r *http.Request) {
-			time.Sleep(delay * time.Millisecond)
+			time.Sleep(delay)
 			w.WriteHeader(http.StatusOK)
 		}))
 }
