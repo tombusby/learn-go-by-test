25171ac015a3af74631a4b8d2afc6411d069d104
Tom Busby <tom@busby.ninja>
2021-08-21 18:38:50 +0200

    Property-Based Tests: Include all the way up to M

--- property-based-tests/roman.go
+++ property-based-tests/roman.go
@@ -7,6 +7,24 @@ func ConvertToRoman(arabic int) string {
 
 	for arabic > 0 {
 		switch {
+		case arabic >= 1000:
+			result.WriteString("M")
+			arabic -= 1000
+		case arabic >= 900:
+			result.WriteString("CM")
+			arabic -= 900
+		case arabic >= 500:
+			result.WriteString("D")
+			arabic -= 500
+		case arabic >= 400:
+			result.WriteString("CD")
+			arabic -= 400
+		case arabic >= 100:
+			result.WriteString("C")
+			arabic -= 100
+		case arabic >= 90:
+			result.WriteString("XC")
+			arabic -= 90
 		case arabic >= 50:
 			result.WriteString("L")
 			arabic -= 50
