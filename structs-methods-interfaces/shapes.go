13575d5963839a434161c2cf86df58026f673b9f
Tom Busby <tom@busby.ninja>
2021-08-21 17:14:01 +0200

    Structs: Set up Perimeter / Area as methods and add Circle type

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -1,14 +1,28 @@
 package shapes
 
+import "math"
+
 type Rectangle struct {
 	Width  float64
 	Height float64
 }
 
-func Permiter(rect Rectangle) float64 {
-	return 2 * (rect.Width + rect.Height)
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
 }
 
-func Area(rect Rectangle) float64 {
-	return rect.Width * rect.Height
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -3,9 +3,9 @@ package shapes
 import "testing"
 
 func TestRectandlePerimeter(t *testing.T) {
-	t.Run("test perimiter of 10 x 10 rect comes out to 40", func(t *testing.T) {
+	t.Run("test perimeter of 10 x 10 rect comes out to 40", func(t *testing.T) {
 		rectangle := Rectangle{10.0, 10.0}
-		got := Permiter(rectangle)
+		got := rectangle.Perimeter()
 		want := 40.0
 
 		if got != want {
@@ -13,9 +13,9 @@ func TestRectandlePerimeter(t *testing.T) {
 		}
 	})
 
-	t.Run("test perimiter of 20 x 3 rect comes out to 46", func(t *testing.T) {
+	t.Run("test perimeter of 20 x 3 rect comes out to 46", func(t *testing.T) {
 		rectangle := Rectangle{20.0, 3.0}
-		got := Permiter(rectangle)
+		got := rectangle.Perimeter()
 		want := 46.0
 
 		if got != want {
@@ -27,7 +27,7 @@ func TestRectandlePerimeter(t *testing.T) {
 func TestRectandleArea(t *testing.T) {
 	t.Run("test area of 10 x 10 rectangle comes out to 100", func(t *testing.T) {
 		rectangle := Rectangle{10.0, 10.0}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 100.0
 
 		if got != want {
@@ -37,11 +37,35 @@ func TestRectandleArea(t *testing.T) {
 
 	t.Run("test area of 20 x 40 rectangle comes out to 800", func(t *testing.T) {
 		rectangle := Rectangle{20.0, 40.0}
-		got := Area(rectangle)
+		got := rectangle.Area()
 		want := 800.0
 
 		if got != want {
 			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+}
+
+func TestCirclePerimeter(t *testing.T) {
+	t.Run("test correct perimeter of r=10 ciricle", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Perimeter()
+		want := 62.83185307179586
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
+}
+
+func TestCircleArea(t *testing.T) {
+	t.Run("test correct area of r=10 ciricle", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
 		}
 	})
 }
