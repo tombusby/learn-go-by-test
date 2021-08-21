987f8300e6020d02c868fe6fb06da954c92ce695
Tom Busby <tom@busby.ninja>
2021-08-21 18:28:25 +0200

    Property-Based Tests: Allow the support of numbers up until 90

--- property-based-tests/roman.go
+++ property-based-tests/roman.go
@@ -7,16 +7,22 @@ func ConvertToRoman(arabic int) string {
 
 	for arabic > 0 {
 		switch {
-		case arabic > 9:
+		case arabic >= 50:
+			result.WriteString("L")
+			arabic -= 50
+		case arabic >= 40:
+			result.WriteString("XL")
+			arabic -= 40
+		case arabic >= 10:
 			result.WriteString("X")
 			arabic -= 10
-		case arabic > 8:
+		case arabic >= 9:
 			result.WriteString("IX")
 			arabic -= 9
-		case arabic > 4:
+		case arabic >= 5:
 			result.WriteString("V")
 			arabic -= 5
-		case arabic > 3:
+		case arabic >= 4:
 			result.WriteString("IV")
 			arabic -= 4
 		default:
