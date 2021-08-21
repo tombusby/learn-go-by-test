3d4127bcc012f46b79b77afb036a5a46c997a4ba
Tom Busby <tom@busby.ninja>
2021-08-21 18:09:05 +0200

    Property-Based Tests: Set up basic ConvertToRoman func that can do 0 < i <= 3

---
+++ property-based-tests/roman_test.go
@@ -0,0 +1,28 @@
+package roman
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRomanNumerals(t *testing.T) {
+
+	cases := []struct {
+		Arabic int
+		Want   string
+	}{
+		{1, "I"},
+		{2, "II"},
+		{3, "III"},
+	}
+
+	for _, test := range cases {
+		t.Run(fmt.Sprintf("%d gets converted to %s", test.Arabic, test.Want),
+			func(t *testing.T) {
+				got := ConvertToRoman(test.Arabic)
+				if got != test.Want {
+					t.Errorf("got %q, want %q", got, test.Want)
+				}
+			})
+	}
+}
