f1689f96be7719f4fd12f8fa285dcb5ae1844620
Tom Busby <tom@busby.ninja>
2021-08-21 16:55:43 +0200

    Structs: Pass a Rectangle to the Area / Perimeter functions

--- structs-methods-interfaces/shapes.go
+++ structs-methods-interfaces/shapes.go
@@ -1,9 +1,14 @@
 package shapes
 
-func Permiter(width float64, height float64) float64 {
-	return 2 * (width + height)
+type Rectangle struct {
+	Width  float64
+	Height float64
 }
 
-func Area(width float64, height float64) float64 {
-	return width * height
+func Permiter(rect Rectangle) float64 {
+	return 2 * (rect.Width + rect.Height)
+}
+
+func Area(rect Rectangle) float64 {
+	return rect.Width * rect.Height
 }
--- structs-methods-interfaces/shapes_test.go
+++ structs-methods-interfaces/shapes_test.go
@@ -2,18 +2,32 @@ package shapes
 
 import "testing"
 
-func TestPerimeter(t *testing.T) {
-	got := Permiter(10.0, 10.0)
-	want := 40.0
+func TestRectandlePerimeter(t *testing.T) {
+	t.Run("test perimiter of 10 x 10 rect comes out to 40", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := Permiter(rectangle)
+		want := 40.0
 
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+
+	t.Run("test perimiter of 20 x 3 rect comes out to 46", func(t *testing.T) {
+		rectangle := Rectangle{20.0, 3.0}
+		got := Permiter(rectangle)
+		want := 46.0
+
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
 
-func TestArea(t *testing.T) {
-	t.Run("test that 10 x 10 comes out to 100", func(t *testing.T) {
-		got := Area(10.0, 10.0)
+func TestRectandleArea(t *testing.T) {
+	t.Run("test area of 10 x 10 rectangle comes out to 100", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := Area(rectangle)
 		want := 100.0
 
 		if got != want {
@@ -21,8 +35,9 @@ func TestArea(t *testing.T) {
 		}
 	})
 
-	t.Run("test that 20 x 40 comes out to 800", func(t *testing.T) {
-		got := Area(20.0, 40.0)
+	t.Run("test area of 20 x 40 rectangle comes out to 800", func(t *testing.T) {
+		rectangle := Rectangle{20.0, 40.0}
+		got := Area(rectangle)
 		want := 800.0
 
 		if got != want {
