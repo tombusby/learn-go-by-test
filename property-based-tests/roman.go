40701255f361be12f58b5d9eaba1e47fb35ee657
Tom Busby <tom@busby.ninja>
2021-08-21 18:15:51 +0200

    Property-Based Tests: Add special cases for 4 and 5

--- property-based-tests/roman.go
+++ property-based-tests/roman.go
@@ -3,9 +3,18 @@ package roman
 import "strings"
 
 func ConvertToRoman(arabic int) string {
+
 	var result strings.Builder
 
-	for i := 0; i < arabic; i++ {
+	for i := arabic; i > 0; i-- {
+		if i == 4 {
+			result.WriteString("IV")
+			break
+		}
+		if i == 5 {
+			result.WriteString("V")
+			break
+		}
 		result.WriteString("I")
 	}
 
--- property-based-tests/roman_test.go
+++ property-based-tests/roman_test.go
@@ -14,6 +14,8 @@ func TestRomanNumerals(t *testing.T) {
 		{1, "I"},
 		{2, "II"},
 		{3, "III"},
+		{4, "IV"},
+		{5, "V"},
 	}
 
 	for _, test := range cases {
