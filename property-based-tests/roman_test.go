8881c638a40fdbbdf04a1e28e097dec165e6ee5d
Tom Busby <tom@busby.ninja>
2021-08-21 20:42:06 +0200

    Property-Based Tests: Finally add the actual property-based test

--- property-based-tests/roman_test.go
+++ property-based-tests/roman_test.go
@@ -3,6 +3,7 @@ package roman
 import (
 	"fmt"
 	"testing"
+	"testing/quick"
 )
 
 type RomanNumeral struct {
@@ -80,5 +81,17 @@ func TestConvertingToArabic(t *testing.T) {
 				t.Errorf("got %d, want %d", got, test.Value)
 			}
 		})
+	}
+}
+
+func TestPropertiesOfConversion(t *testing.T) {
+	assertion := func(arabic uint16) bool {
+		roman := ConvertToRoman(arabic)
+		fromRoman := ConvertToArabic(roman)
+		return fromRoman == arabic
+	}
+
+	if err := quick.Check(assertion, nil); err != nil {
+		t.Error("failed checks", err)
 	}
 }
